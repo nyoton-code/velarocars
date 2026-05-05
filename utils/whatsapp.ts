e08e1063ff2b57985f318db0a1343a64bbad00e3
nyoton <dibazakaria098@gmail.com>
2026-05-05 02:00:43 +0100

    Update whatsapp.ts

--- utils/whatsapp.ts
+++ utils/whatsapp.ts
@@ -1,5 +1,5 @@
 export const generateWhatsAppLink = (carName: string) => {
-  const phone = "+212689073517"; 
+  const phone = "+212681117195"; 
   const message = `Hello, I want to book the ${carName}.`;
 
   return `https://wa.me/${phone}?text=${encodeURIComponent(message)}`;
